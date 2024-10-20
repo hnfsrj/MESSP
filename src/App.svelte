9e7bbfe133ef2c3db945284687ad76d683d490f8
Hanif <hnfsrj@gmail.com>
2024-10-20 16:54:08 +0300

    initialize

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+
+	import Nav from './components/Nav.svelte';
+	import Landing from './components/Landing.svelte';
+	import About from './components/About.svelte';
+	import Vision from './components/Vision.svelte';
+	import Services from './components/Services.svelte';
+	import Distinction from './components/Distinction.svelte';
+	import Approach from './components/Approach.svelte';
+	import Contact from './components/Contact.svelte';
+
+</script>
+
+
+<div id="container">
+	<Nav/>
+	<Landing/>
+	<About/>
+	<Vision/>
+	<Services/>
+	<Distinction/>
+	<Approach/>
+	<Contact/>
+</div>
+
+
+<style>
+
+</style>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,84 @@
+<script>
+
+</script>
+
+
+<nav>
+    <div class="top">
+        <div class="logo img"></div>
+        <div class="menu img"></div>
+        <div class="close img hide"></div>
+    </div>
+
+    <div class="bottom">
+        
+        <p>About</p>
+        <p>Vision</p>
+        <p>Services</p>
+        <p>Distinction</p>
+        <p>Appoach</p>
+        <p>Contact</p>
+        
+    </div>
+
+</nav>
+
+
+<style>
+
+    nav{
+        position:fixed;
+        width:100%;
+        background:red;
+        box-sizing: border-box;;
+    }
+
+        .top{
+            background:orange;
+            display:flex;
+            justify-content: space-between;
+            align-items: center;
+            padding:10px 16px;
+        }
+
+            .logo{
+                width:35px;
+                aspect-ratio: 1/1;
+                background-image:url("/images/logo.svg");
+            }
+
+            .menu{
+                width:25px;
+                aspect-ratio: 1/1;
+                background-image:url("/images/menu.svg");
+            }
+
+            .close{
+                width:50px;
+                aspect-ratio: 1/1;
+                background-image:url("/images/close.svg");
+            }
+
+        .bottom{
+            padding:15px 16px;
+            background:rgba(255,255,255,0.75);
+
+            display:flex;
+            flex-wrap:wrap;
+            flex-direction:column;
+            gap:5px;
+        }
+
+            p{
+                font-size:1.6rem;
+            }
+
+</style>
+
+
+
+
+
+
+
+
