115de7e1dacb9a957624fea442e5c02e1fe87d53
Hanif <hnfsrj@gmail.com>
2024-10-21 15:41:56 +0300

    complete initial look for small devices

---
+++ src/components/Contact.svelte
@@ -0,0 +1,113 @@
+
+
+<script>
+
+    import ContactForm from './ContactForm.svelte';
+</script>
+
+<section id="contact">
+
+    <p>Contact Us</p>
+
+    <p>Email, call or complete the form to learn how we can provide you with premium engineering solutions tailored to your needs.</p>
+
+    <p>E-MAIL: adamantexport@gmail.com</p>
+
+    <div class="info">
+        <div>Head Office</div>
+
+        <p>MOB: +251 911 24 64 75</p>
+        <p>OFF: +251 114 70 29 11</p>
+        <p>GOFA GEBRIEL JFK BUILDING 3RD</p>
+        <p>FLOOR P.O.BOX 8853</p>
+        <p>ADDIS ABABA, ETHIOPIA</p>
+    </div>
+
+
+    <div class="info">
+        <div>Dubain Branch</div>
+
+        <p>MOB: +971 50 968 1540</p>
+        <p>OFF: +971 4 345 7172</p>
+        <p>DUBAI SHOPPING CENTRE, 1ST FLOOR</p>
+        <p>P.O.BOX 251006</p>
+        <p>DUBAI, UAE</p>
+
+    </div>
+
+
+    <ContactForm />
+
+    <h6>© 2024 Adamant Engineering.<br/>All Rights Reserved.</h6>
+
+</section>
+
+<style>
+
+    #contact{
+        background:var(--blue);
+    }
+
+        #contact>p:first-of-type{
+            color:var(--green);
+            font-weight:700;
+            font-size:3rem;
+            margin-bottom:20px;
+        }
+
+
+        #contact>p:nth-of-type(2){
+            color:white;
+            font-weight:500;
+            font-size:1.6rem;
+            margin-bottom:20px;
+        }
+
+
+        #contact>p:last-of-type{
+            color:white;
+            font-weight:500;
+            font-size:1.4rem;
+            margin-bottom:40px;
+        }
+
+
+        .info{
+                color:white;
+                width:100%;
+                margin-bottom:40px;
+        }
+
+            .info>div{
+                color:white;
+                background:var(--green);
+                border-radius:13px;
+                width:50%;
+                padding:7px 0px;
+                display:flex;
+                justify-content: center;
+                align-items: center;
+                font-size:1.5rem;
+                font-weight:600;
+                margin-bottom:15px;
+
+            }
+
+            .info p{
+                font-size:1.5rem;
+                font-weight:500;
+            }
+
+
+    h6{
+        color:white;
+        margin-top:60px;
+        font-size:1.4rem;
+        font-weight:500;
+        text-align:center;
+
+    }
+
+
+
+</style>
---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,79 @@
+
+<script>
+
+</script>
+
+<div class="form_container">
+
+    <p>Get in Touch</p>
+
+    <form>
+
+        <input type="text" placeholder="Name"/>
+        <input type="email" placeholder="Email"/>
+        <input type="text" placeholder="Subject"/>
+        <textarea placeholder="Message"></textarea>
+
+        <input type="submit" value="Submit" />
+    </form>
+
+
+</div>
+
+<style>
+
+    .form_container{
+        background:white;
+        font-family:"itim";
+        box-sizing: border-box;
+        width:100%;
+        border-radius:10px;
+        box-shadow: 0px 0px 4px 2px rgba(0,0,0,0.25);
+        padding:20px 10px;
+        font-size:1.6rem;
+        
+    }
+
+        .form_container>p{
+            margin-bottom:15px;
+        }
+
+        form{}
+
+            form input:not([type="submit"]){
+                border:2px solid black;
+                border-radius:10px;
+                width:100%;
+                box-sizing: border-box;
+                padding:0px 10px;
+                height:40px;
+                margin-bottom:10px;
+            }
+
+            form textarea{
+                border:2px solid black;
+                border-radius:10px;
+                box-sizing: border-box;
+                padding:5px 10px;
+                width:100%;
+                resize: none;
+                height:100px;
+            }
+
+            form input[type="submit"]{
+                display: block;
+                color:white;
+                background:var(--green);
+                outline:none;
+                border:none;
+                box-shadow: 0px 3px 3px 1px rgba(0,0,0,0.25);
+                width:80%;
+                margin:0 auto;
+                margin-top:30px;
+                border-radius:15px;
+                height:40px;
+                font-size:2rem;
+                font-weight:700;
+            }
+
+</style>
