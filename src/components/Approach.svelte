16d83fc70fbaa06bf470994d87effdc458b991f6
Hanif <hnfsrj@gmail.com>
2024-10-21 14:24:39 +0300

    add adamant's way

---
+++ src/components/Approach.svelte
@@ -0,0 +1,90 @@
+
+
+<script>
+
+
+</script>
+
+
+<section id="approach">
+
+    <p>Adamant's Way</p>
+
+    <div class="ways_container">
+        <div class="way">
+
+            <img src="/images/knowledge.svg" />
+
+            <p>Knowledge Transfer</p>
+
+            <p>We believe in sharing knowledge and building skills. Adamant Engineering brings expertise from abroad to Ethiopia, driving innovation and helping our local workforce grow stronger.</p>
+        
+        </div>
+
+        <div class="way">
+
+            <img src="/images/client.svg" />
+
+            <p>Client Collaboration</p>
+
+            <p>We work closely with our clients, helping them set up branches and expand their reach. Together, we grow and succeed, creating lasting partnerships.</p>
+        
+        </div>
+
+
+        <div class="way">
+
+            <img src="/images/future.svg" />
+
+            <p>Future Outlook</p>
+
+            <p>Looking ahead, Adamant Engineering is committed to pushing innovation, increasing collaboration, and supporting Ethiopia’s journey toward sustainable development.</p>
+        
+        </div>  
+    </div>
+
+
+</section>
+
+
+<style>
+
+    #approach{}
+
+        #approach > p{
+            text-align:center;
+            font-size:2.2rem;
+            font-weight:700;
+            color:var(--blue);
+            margin-bottom:40px;
+        }
+
+        .ways_container{
+            width:100%;
+        }
+
+            .way{
+                text-align:center;
+                margin-bottom:40px;
+            }
+
+                .way > img{
+                    width:50px;
+                }
+
+
+                .way > p:first-of-type{
+                    color:var(--blue);
+                    font-size:1.7rem;
+                    font-weight:700;
+                    margin-top:10px;
+                    margin-bottom:15px;
+                }
+
+                .way > p:last-of-type{
+                    color:black;
+                    font-size:1.3rem;
+                    font-weight:500;
+                }
+
+</style>
