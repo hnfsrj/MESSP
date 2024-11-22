d8483c264d624b8088186dac8e11fc95d0b9714a
Hanif <hnfsrj@gmail.com>
2024-11-22 04:05:28 +0300

    remove width printer

--- src/App.svelte
+++ src/App.svelte
@@ -13,15 +13,15 @@
 	import Industrial from './Industrial.svelte';
 	import Refrigeration from './Refrigeration.svelte';
 
-	function resize_handler(){
-		let width = window.innerWidth;
+	// function resize_handler(){
+	// 	let width = window.innerWidth;
 
-		console.log(width);
-	}
+	// 	console.log(width);
+	// }
 
 
-	resize_handler();
-	window.addEventListener("resize", resize_handler);
+	// resize_handler();
+	// window.addEventListener("resize", resize_handler);
 
 </script>
 
